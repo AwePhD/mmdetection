989be41fa8925dba7e799ea0cc2c8aa14fa7df61
AwePhD <mathias.reus@gipsa-lab.fr>
2024-03-01 11:41:40 +0100

    feat(evalulate): dump D-ReID results
    The evaluation is done in two times:
    
    1. Process results to pickle file
    2. Evaluate results
    
    It's handy to manage new metrics separetely and GPU can be used
    efficiently and CPU might use workers to evaluate fastly.
    Also, introduce specific "tools" test script.

--- mmdet/evaluation/metrics/__init__.py
+++ mmdet/evaluation/metrics/__init__.py
@@ -9,6 +9,7 @@ from .coco_video_metric import CocoVideoMetric
 from .crowdhuman_metric import CrowdHumanMetric
 from .dump_det_results import DumpDetResults
 from .dump_proposals_metric import DumpProposals
+from .dump_reid_det_results import DumpReIDDetResults
 from .lvis_metric import LVISMetric
 from .mot_challenge_metric import MOTChallengeMetric
 from .openimages_metric import OpenImagesMetric
@@ -25,4 +26,5 @@ __all__ = [
     'CocoOccludedSeparatedMetric', 'DumpDetResults', 'BaseVideoMetric',
     'MOTChallengeMetric', 'CocoVideoMetric', 'ReIDMetrics', 'YouTubeVISMetric',
     'COCOCaptionMetric', 'SemSegMetric', 'RefSegMetric', 'ReIDDetMetric',
+    'DumpReIDDetResults'
 ]
